0a9e2af620259cd19df9af5221b61df49d4277a7
Yoel Trujillo <yoeltrujillo@MacBook-Pro-de-Yoel.local>
2020-07-10 02:43:58 -0400

    Docs agregados

---
+++ docs/precache-manifest.e1db61ec5203da382425546c1547b8bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fda824e196dddc7c96cf44cccc49485",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15e2a59762c69492ecba",
+    "url": "/static/css/main.06d71cf1.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e2a59762c69492ecba",
+    "url": "/static/js/main.54731ea1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
